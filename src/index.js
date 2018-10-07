181536b84a22bfb24aa8a1beb8fe146e13dfc60d
marksabelita <marksabelita@gmail.com>
2018-10-07 10:59:35 +0800

    Done with creating react router

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux';
 import { Route } from "react-router";
 import { ConnectedRouter } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
-import { store } from './store';
+import store from './store';
 
+import Test from './components/test';
 import App from './components/app';
 
 const history = createHistory();
@@ -15,6 +16,7 @@ ReactDOM.render(
     <ConnectedRouter history={history}>
       <div>
         <Route exact path="/" component={App} />
+        <Route exact path="/test" component={Test} />
       </div>
     </ConnectedRouter>
   </Provider>, document.getElementById('app')
