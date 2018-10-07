4eaa602969188c8c41ce9d1347ab91c411be81bb
marksabelita <marksabelita@gmail.com>
2018-10-07 13:58:49 +0800

    Fixed webpack and package.json and mocha test

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -6,7 +6,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /test/],
         loaders: ['eslint-loader'],
       },
       {
